2bd03023e23e08c97452982d7786fe4c84673c51
Dongmin Kim <minithelife@gmail.com>
2025-10-31 16:51:58 -0600

    Phase 3 fix userDAO for compiling issues

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -3,9 +3,8 @@ package dataaccess;
 import model.UserData;
 
 public interface UserDAO {
-
     void createUser(UserData user);
     UserData getUser(String username);
-
     void clear();
+    boolean userExists(String username); // add this
 }
