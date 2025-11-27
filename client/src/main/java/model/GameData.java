e145976718224963190af261b54f261845061483
loaner <minithelife@gmail.com>
2025-11-27 00:12:17 -0700

    Phase 5 gamedata create

---
+++ client/src/main/java/model/GameData.java
@@ -0,0 +1,24 @@
+package model;
+
+import chess.ChessGame;
+
+public record GameData(
+        int gameID,
+        String gameName,
+        String whiteUsername,
+        String blackUsername,
+        ChessGame game
+) {
+
+    public GameData withWhite(String username) {
+        return new GameData(gameID, gameName, username, blackUsername, game);
+    }
+
+    public GameData withBlack(String username) {
+        return new GameData(gameID, gameName, whiteUsername, username, game);
+    }
+
+    public GameData withGame(ChessGame newGame) {
+        return new GameData(gameID, gameName, whiteUsername, blackUsername, newGame);
+    }
+}
