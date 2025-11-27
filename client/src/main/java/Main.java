de2ec50fbbdd267036c8dba35dff6fa63e19a419
loaner <minithelife@gmail.com>
2025-11-27 00:12:41 -0700

    Phase 5 main create

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,25 @@
-import chess.*;
+//import chess.*;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Client: " + piece);
+//    }
+//}
+
+
+
+import client.Client;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        String serverUrl = "http://localhost:8080";
+        if (args.length == 1) serverUrl = args[0];
+
+        try {
+            new Client(serverUrl).run();
+        } catch (Throwable ex) {
+            System.out.printf("Unable to start client: %s%n", ex.getMessage());
+        }
     }
 }
